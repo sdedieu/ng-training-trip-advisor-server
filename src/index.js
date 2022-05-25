4824722c167977750da23785a2e13f0b2ea5c388
sdedieu <dedieu.sylvain38@gmail.com>
2022-05-25 16:21:43 +0200

    add more hotels

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
-const {restaurants} = require("./restaurants");
-const {hotels} = require("./hotels");
+const { restaurants } = require("./restaurants");
+const { hotels } = require("./hotels");
 const app = express();
 app.use(cors());
 const port = 3000;
@@ -20,31 +20,32 @@ const COMMON_BASE_ROUTE = "/api";
 const RESTAURANTS_ROUTE = "restaurants";
 const HOTELS_ROUTE = "hotels";
 
-
 app.get("/", (req, res) => {
   res.send("Hello Kaizeners!");
 });
 
 app.get(`${COMMON_BASE_ROUTE}/${RESTAURANTS_ROUTE}`, (req, res) => {
-    if(req?.query?.urgent === 'true'){
-       return res.status(417).send('Error, urgent things always fail!');
-    }
-  const _restaurants = restaurants
-    .filter(
-      (r) =>
-        !req?.query?.search || r.name.toLowerCase().includes(req?.query?.search)
-    )
-    .map((r) => ({
-      ...r,
-      image: base64_encode(`src/images/restaurants/${r.id}.jpg`),
-    }));
-  res.send(_restaurants);
+  if (req?.query?.urgent === "true") {
+    return res.status(417).send("Error, urgent things always fail!");
+  }
+  
+    const _restaurants = restaurants
+      .filter(
+        (r) =>
+          !req?.query?.search ||
+          r.name.toLowerCase().includes(req?.query?.search)
+      )
+      .map((r) => ({
+        ...r,
+        image: base64_encode(`src/images/restaurants/${r.id}.jpg`),
+      }));
+    res.send(_restaurants);
 });
 
 app.get(`${COMMON_BASE_ROUTE}/${RESTAURANTS_ROUTE}/:id`, (req, res) => {
-    if(req?.query?.urgent === 'true'){
-       return res.status(417).send('Error, urgent things always fail!');
-    }
+  if (req?.query?.urgent === "true") {
+    return res.status(417).send("Error, urgent things always fail!");
+  }
   const _restaurant = {
     ...restaurants.find((r) => r.id === req.params.id),
     image: base64_encode(`src/images/restaurants/${req.params.id}.jpg`),
@@ -53,32 +54,44 @@ app.get(`${COMMON_BASE_ROUTE}/${RESTAURANTS_ROUTE}/:id`, (req, res) => {
 });
 
 app.get(`${COMMON_BASE_ROUTE}/${HOTELS_ROUTE}`, (req, res) => {
-    if(req?.query?.urgent === 'true'){
-       return res.status(417).send('Error, urgent things always fail!');
-    }
-  const _hotels = hotels
-    .filter(
-      (r) =>
-        !req?.query?.search || r.name.toLowerCase().includes(req?.query?.search)
-    )
-    .map((r) => ({
-      ...r,
-      image: base64_encode(`src/images/hotels/${r.id}.jpg`),
-    }));
-  res.send(_hotels);
+  if (req?.query?.urgent === "true") {
+    return res.status(417).send("Error, urgent things always fail!");
+  }
+  if (!req?.query?.from || !req?.query?.to) {
+    return res.status(405).send("Error, range has to be provided!");
+  }
+
+  setTimeout(() => {
+    const _hotels = hotels
+      .filter(
+        (h) =>
+          !req?.query?.search ||
+          h.name.toLowerCase().includes(req?.query?.search)
+      )
+      .sort((a, b) => a.id - b.id)
+      .filter(
+        (h, i, hs) =>
+          i >= Number(req?.query?.from) && i <= Number(req?.query?.to)
+      )
+      .map((h) => ({
+        ...h,
+        image: base64_encode(`src/images/hotels/${h.id}.jpg`),
+      }));
+    res.send({items: _hotels, total: hotels.length});
+  }, 2000);
 });
 
 app.get(`${COMMON_BASE_ROUTE}/${HOTELS_ROUTE}/:id`, (req, res) => {
-    if(req?.query?.urgent === 'true'){
-       return res.status(417).send('Error, urgent things always fail!');
-    }
+  if (req?.query?.urgent === "true") {
+    return res.status(417).send("Error, urgent things always fail!");
+  }
   const _hotels = {
-    ...restaurants.find((r) => r.id === req.params.id),
+    ...restaurants.find((h) => h.id === req.params.id),
     image: base64_encode(`src/images/hotels/${req.params.id}.jpg`),
   };
   res.send(_hotels);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Backend app listening on port ${port}`);
 });
